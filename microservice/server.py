9b9eb2182032ba1318206d299a6f2ea4c499e8e0
ExtremCode <lev-moshkin@yandex.ru>
2025-01-12 11:15:03 +0300

    improve model results, define final model

--- microservice/server.py
+++ microservice/server.py
@@ -2,19 +2,29 @@ from flask import Flask, jsonify, request
 from joblib import load
 import numpy as np
 from flask_cors import CORS
+from xbg import DMatrix
 
 app = Flask(__name__)
 
 # Cross Origin Resource Sharing (CORS) handling
 CORS(app, resources={'/smart': {"origins": "http://localhost:8080"}})
 
-model = load("fitted_random_forest.pkl")
+model = load("fitted_xgb.pkl")
 
 @app.route('/smart', methods=['POST'])
 def image_post_request():
     x = np.array(request.json['raw_smart'])
-    y = model.predict_proba(x)[:, 1][0]
-    failure = f"broke within 30d with prob: {y}" if y > 0.5 else f"good with prob: {1 - y}"
+    smart_192cumul = x[-7] / x[1]
+    smart_240_div_9 = x[-3] / x[1]
+    smart_197_div_241 = x[-6] / x[-2]
+    smart_198_div_241 = x[-5] / x[-2]
+    smart_242_div_241 = x[-1] / x[-2]
+    data = np.concatenate([
+        x, smart_192cumul, smart_240_div_9, smart_197_div_241,
+        smart_198_div_241, smart_242_div_241
+    ])
+    y = model.predict(DMatrix(data))[0]
+    failure = f"broke within 30d with prob: {y}" if y > 0.5 else f"good with prob: {round(1 - y, 5)}"
 
     return jsonify({'result': failure})
 
